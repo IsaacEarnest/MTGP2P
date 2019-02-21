3153b16ee885cbce575caaa980c09f44c7a7650b
IsaacEarnest <earnestia@hendrix.edu>
2019-02-20 21:31:15 -0600

    u

--- app/src/main/java/com/example/mtg/MainActivity.java
+++ app/src/main/java/com/example/mtg/MainActivity.java
@@ -10,11 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try {
-            myIpView.setText(Utilities.getLocalIpAddress());
-        } catch (SocketException e) {
-            Log.e("MainActivity", "Threw exception when finding ip address");
-        }
+        
 
         gameStart();
     }

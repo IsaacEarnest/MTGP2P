ba09d983203a6ed6d94e6a4f917c558f02d337dd
David Pojunas <davidapojunas@gmail.com>
2019-02-27 11:08:21 -0600

    dfghj

--- app/src/main/java/com/example/mtg/activities/ActivityChooseDeck.java
+++ app/src/main/java/com/example/mtg/activities/ActivityChooseDeck.java
@@ -1,16 +1,45 @@
 package com.example.mtg.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mtg.R;
 
 public class ActivityChooseDeck extends AppCompatActivity {
 
+    private Button clickRedDeck;
+    private Button clickBlueDeck;
+    private Button clickPlayGame;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_deck2);
+        clickRedDeck = findViewById(R.id.choose_redDeck);
+        clickBlueDeck = findViewById(R.id.choose_blueDeck);
+        clickPlayGame = findViewById(R.id.chooseDeck_PlayGame);
+        clickPlayGame.setVisibility(View.INVISIBLE);
 
+    }
+
+    private void openActivityGameBoard() {
+        Intent intent = new Intent(this, ActivityGameBoard.class);
+        startActivity(intent);
+    }
+
+    public void clickRedDeck(View view) {
+        clickPlayGame.setVisibility(View.VISIBLE);
+
+    }
+
+    public void clickBlueDeck(View view) {
+        clickPlayGame.setVisibility(View.VISIBLE);
+    }
+
+    public void clickPlayGame(View view) {
+        openActivityGameBoard();
     }
 }
--- app/src/main/java/com/example/mtg/activities/ActivityGameBoard.java
+++ app/src/main/java/com/example/mtg/activities/ActivityGameBoard.java
@@ -1,5 +1,6 @@
 package com.example.mtg.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -27,6 +28,8 @@ public class ActivityGameBoard extends AppCompatActivity {
 
 
     }
+
+
 
 
 }
